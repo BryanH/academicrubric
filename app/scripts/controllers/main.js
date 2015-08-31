89522b9e005481207825b046f621749439f5faca
Bryan Hanks, PMP <bryan@master-developer.com>
2015-08-31 15:30:45 -0500

    Added HESI and TEAS functionality; only thing left is humanities and printing/saving

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,7 @@ angular.module('adrateApp')
       'Karma'
     ];
   })
-.controller('RatingController', function($scope) {
+.controller('RatingController', ['$scope', '$rootScope', function( $scope, $rootScope ) {
 					$scope.classes = [
 						{name: 'Engl 1301', value:0, has_points: true},
 						{name: 'Biol 2401', value:0, has_points: true},
@@ -25,14 +25,32 @@ angular.module('adrateApp')
 						{name: 'Biol 2420', value:0, has_points: true},
 						{name: 'Psyc 2314', value:0, has_points: true}
 					];
+					$scope.hesis = [
+						{name: "AP", value:0},
+						{name: "G", value:0},
+						{name: "M", value:0},
+						{name: "RC", value:0},
+						{name: "Date", value:0, is_date: true}
+					];
+					$scope.teases = [
+						{name: "R", value:0},
+						{name: "M", value:0},
+						{name: "S", value:0},
+						{name: "E", value:0},
+						{name: "Date", value:0, is_date: true}
+					];
 					$scope.bachelors = { name: 'bachelors', value:false };
 
 					$scope.academicTotal = 0;
+					$scope.teasTotal = 0;
+					$scope.hesiTotal = 0;
 					$scope.combinedTotal = 0;
-					$scope.program = "Associate Degree Nursing";
+					$scope.showHesiWarning = true;
+
+					$rootScope.pageTitle = "Associate Degree Nursing";
 					//		$scope.points = { startingPoints: 0 };
 
-									// TODO - calculate
+									// calculate academic
 									$scope.computeAcademic = function() {
 										var total = 0;
 										// Loop through all values
@@ -46,13 +64,94 @@ angular.module('adrateApp')
 										$scope.computeTotal();
 									};
 
+									// calculate hesi
+									$scope.computeHesi = function() {
+										var total = 0;
+										var warning = true;
+										for( var i = 0, len = $scope.hesis.length; i < len; i++ ) {
+											// Skip date field
+											if( $scope.hesis[i].is_date ) {
+												continue;
+											}
+											// No HESI if any scores < 75
+											if( $scope.hesis[i].value < 75 ) {
+												warning = true;
+												total = 0;
+												break;
+											}
+											// If we get here, we're okay
+											warning = false;
+											total += hesiPoints( $scope.hesis[i].value );
+										}
+										$scope.hesiTotal = total;
+										$scope.showHesiWarning = warning;
+										$scope.computeTotal();
+									}
+
+									// calculate teas
+									$scope.computeTeas = function() {
+										var total = 0;
+										for( var i = 0, len = $scope.teases.length; i < len; i++ ) {
+											// Skip date field
+											if( $scope.teases[i].is_date ) {
+												continue;
+											}
+
+											total += teasPoints( $scope.teases[i].value );
+										}
+										$scope.teasTotal = total;
+										$scope.computeTotal();
+									}
+
 									$scope.computeTotal = function() {
 										var total = 0;
+										total += $scope.hesiTotal;
+										total += $scope.teasTotal;
 										if( $scope.bachelors.value) {
 											total++;
 										}
 										total += $scope.academicTotal;
 										$scope.combinedTotal = total;
+									}
+
+									/*
+									 Calculate points from HESI Scores
+									*/
+									function hesiPoints( hesiPercent ) {
+										// Can't use a switch statement here
+										var points = 0;
+										if( hesiPercent >= 90 ) {
+											points = 3;
+										} else {
+											if( hesiPercent >= 80 ) {
+												points = 2;
+											} else {
+												// below 80 is zero points
+											}
+										}
+										return points;
+									}
+
+									/*
+									 Calculate points from TEAS scores
+									*/
+									function teasPoints( teasPercent ) {
+										// Can't use a switch statement here
+										var points = 0;
+										if( teasPercent >= 83 ) {
+											points = 3;
+										} else {
+											if( teasPercent >= 74 ) {
+												points = 2;
+											} else {
+												if( teasPercent >= 64 ) {
+													points = 1
+												} else {
+													// below 64 is zero points
+												}
+											}
+										}
+										return points;
 									}
 /*
 							$scope.class.points.total = function() {
@@ -63,4 +162,4 @@ angular.module('adrateApp')
 									return total;
 									}
 									*/
-});
+}]);
